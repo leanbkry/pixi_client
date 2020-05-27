176a0396fde5c3be7dc97b322fcae59dc2d7aaca
Florian Bergmann <florian.bergmann@leanbakery.com>
2020-05-27 17:58:06 +0200

    ImportAddXML Request

---
+++ lib/pixi_client/requests/import_add_xml.rb
@@ -0,0 +1,21 @@
+module PixiClient
+  module Requests
+    class ImportAddXML < Base
+      attr_accessor :xml
+
+      def initialize(xml, operationType, channelRef)
+        self.xml = xml
+        self.operationType = xml
+        self.channelRef = channelRef
+      end
+
+      def api_method
+        :pixi_import_add_xml
+      end
+
+      def message
+        { 'ChannelRef' => channelRef, 'OperationType' => operationType, 'XML' => xml }
+      end
+    end
+  end
+end
